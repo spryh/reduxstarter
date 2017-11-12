56ccde2278b2b706b0df5c143224eef7ef3f2d69
Spryh <skyphr@gmail.com>
2017-11-12 13:43:24 -0800

    Through Lecture 31 - Video Search working, PrettierJS, CSS

--- src/App.js
+++ src/App.js
@@ -1,69 +1,54 @@
-import React, { Component } from 'react'; //Libraries dont need paths, CRA imports Component
-import './App.css';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import YTSearch from 'youtube-api-search';
-import VideoDetail from './components/video_detail';
+import React, { Component } from 'react'
+import './App.css'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search'
+import VideoDetail from './components/video_detail'
 
-const API_KEY = 'AIzaSyBwnNqbRoNVMBvT6_8VY4pJ7pYzwAh6QAk';
+const API_KEY = 'AIzaSyBwnNqbRoNVMBvT6_8VY4pJ7pYzwAh6QAk'
 
-
-// const App = function(){
-// We can arrow the function in ES6
-
-class App extends Component{
-    //parenthesis for multi line returns
-
-    constructor(props){
-        super(props);
-
-        this.state = {
-            app_videos:[], // local app videos
-            selected_video: null
-        };
-        YTSearch(
-            {key:API_KEY, term:'surfboards'},
-            (yt_videos) => {
-                this.setState({
-                    app_videos:yt_videos,
-                    selected_video: yt_videos[0]
-                })
-            }
-        );
-    }
-//make sure to pass setState an object {}! on this multi-level callback!
-    render () {
-        return (
-            <div>
-                <SearchBar />
-                <VideoDetail vli_video={this.state.selected_video}/>
-                <VideoList
-                    vl_videos = {this.state.app_videos}
-                    onVideoSelect={(selected_video) => this.setState({selected_video})}
-                />
-            </div>
-        );
-    }
-}
-export default App;
-
-
-
-// Let's comment out the boilerplate from CRA for now...
-/*
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get finished, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+   constructor(props) {
+      super(props)
+
+      this.state = {
+         app_videos: [], // local app videos
+         selected_video: null
+      }
+
+      this.videoSearch('surf')
+   }
+
+   videoSearch(search_term) {
+      YTSearch(
+         {
+            key: API_KEY,
+            term: search_term
+         },
+         yt_videos => {
+            this.setState({
+               app_videos: yt_videos,
+               selected_video: yt_videos[0]
+            })
+         }
+      )
+   }
+
+   render() {
+      return (
+         <div>
+            <SearchBar
+               onSearchChange={search_term => this.videoSearch(search_term)}
+            />
+            <VideoDetail vli_video={this.state.selected_video} />
+            <VideoList
+               vl_videos={this.state.app_videos}
+               onVideoSelect={selected_video =>
+                  this.setState({ selected_video })
+               }
+            />
+         </div>
+      )
+   }
 }
-*/
+export default App
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,48 +1,26 @@
-import React, {Component} from 'react'; //import React, pull React.Component as named variable
-
-/* This would be the simple function approach
-
-const SearchBar = () => {
-    return <input/>
-};
-*/
-
+import React, { Component } from 'react' //import React, pull React.Component as named variable
 
 class SearchBar extends Component {
-    // all classes have a constructor object
-    // it initializes
-    // "super" calls the inherited class
-    // "term" is a property we created to record state of search term
+   constructor(props) {
+      super(props)
+      this.state = { term: '' }
+   }
 
-    constructor(props) {
+   onInputChange(search_term) {
+      this.setState({ search_term })
+      this.props.onSearchChange(search_term)
+   }
 
-        super(props);
-        this.state = { term: ''};
-        //state should only be touched this way inside a constructor
-
-    }
-
-    //controlled components have values saved by state
-    render() {
-        return (
-            <div>
-                <input
-                    value={this.state.term}
-                    onChange={event => this.setState({ term: event.target.value})}
-                />
-                {/*Value of the Input: {this.state.term}*/}
-            </div>
-        );
-    }
-
-    /*return <input onChange=event => console.log(event.target.value)}/>;
-    return <input onChange={event => this.setState({ term: event.target.value})}/>
-
-    Event handler .. "on element event" naming convention
-    onInputChange(event) {
-        //console.log(event.target.value);
-        this.setState({ term: event.target.value})
-    }*/
+   render() {
+      return (
+         <div className="search-bar">
+            <input
+               value={this.state.search_term} //defines the search term variable
+               onChange={event => this.onInputChange(event.target.value)}
+            />
+         </div>
+      )
+   }
 }
 
-export default SearchBar;
+export default SearchBar
