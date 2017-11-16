d3d444f24f481c70900013b905b6fa0e95ec2fe3
Spryh <skyphr@gmail.com>
2017-11-15 17:17:15 -0800

    Through Lecture 33 - Used Lodash bounce to throttle searches

--- src/App.js
+++ src/App.js
@@ -4,51 +4,53 @@ import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import YTSearch from 'youtube-api-search'
 import VideoDetail from './components/video_detail'
+import _ from 'lodash'
 
 const API_KEY = 'AIzaSyBwnNqbRoNVMBvT6_8VY4pJ7pYzwAh6QAk'
 
 class App extends Component {
-   constructor(props) {
-      super(props)
+  constructor(props) {
+    super(props)
 
-      this.state = {
-         app_videos: [], // local app videos
-         selected_video: null
+    this.state = {
+      app_videos: [], // local app videos
+      selected_video: null
+    }
+
+    this.videoSearch('surf')
+  }
+
+  videoSearch(search_term) {
+    YTSearch(
+      {
+        key: API_KEY,
+        term: search_term
+      },
+      yt_videos => {
+        this.setState({
+          app_videos: yt_videos,
+          selected_video: yt_videos[0]
+        })
       }
+    )
+  }
 
-      this.videoSearch('surf')
-   }
+  render() {
+    // throttle the videoSearch function to once ever 300ms using Lodash Debounce
+    const videoSearch = _.debounce(search_term => {
+      this.videoSearch(search_term)
+    }, 300)
 
-   videoSearch(search_term) {
-      YTSearch(
-         {
-            key: API_KEY,
-            term: search_term
-         },
-         yt_videos => {
-            this.setState({
-               app_videos: yt_videos,
-               selected_video: yt_videos[0]
-            })
-         }
-      )
-   }
-
-   render() {
-      return (
-         <div>
-            <SearchBar
-               onSearchChange={search_term => this.videoSearch(search_term)}
-            />
-            <VideoDetail vli_video={this.state.selected_video} />
-            <VideoList
-               vl_videos={this.state.app_videos}
-               onVideoSelect={selected_video =>
-                  this.setState({ selected_video })
-               }
-            />
-         </div>
-      )
-   }
+    return (
+      <div>
+        <SearchBar onSearchChange={videoSearch} />
+        <VideoDetail vli_video={this.state.selected_video} />
+        <VideoList
+          vl_videos={this.state.app_videos}
+          onVideoSelect={selected_video => this.setState({ selected_video })}
+        />
+      </div>
+    )
+  }
 }
 export default App
